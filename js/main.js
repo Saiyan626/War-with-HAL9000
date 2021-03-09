507e34557150e825662eacad1638fff02d6c94da
Urian Muro <urianmuro@Urians-MacBook-Pro.local>
2021-03-08 16:13:07 -0800

    css card deck files added need JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
+//CPUDeck = Reset button = Shuffles deck in half to start game
+//PlayerDeck = Draw button = Draws card from each players deck
+//DRAW = Player w/ > cardValue = +2cards else -1cards 
+//DrawTieWin = +6 else -3
+
 /*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
-
+const players = {
+    player1 = {
+        cardsInDeck: 26,
+        cardDrawn: 0 
+    }
+    computer = {
+        cardsInDeck: 26,
+        cardDrawn: 0
+    }
+};
+const points = "";
 
 /*----- app's state (variables) -----*/
-
-
+const cards, players, points;
 
 /*----- cached element references -----*/
 
-
-
 /*----- event listeners -----*/
+//draw button to draw cards from both players for values to be compared.
+document.querySelector()
+.addEventListener("click, ");
 
-
-
+document.querySelector()
+.addEventListener("click, ");
 /*----- functions -----*/
-
-
